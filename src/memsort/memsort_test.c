66281df5253238b3df8681d4f3b512ef0cedcbcd
benheberlein <ben.heberlein@gmail.com>
2017-09-17 14:37:04 -0600

    Modified build_kernel debug, added delete_kernel.sh script, added memsort_test.c, and modified memsort to print correctly

---
+++ src/memsort/memsort_test.c
@@ -0,0 +1,42 @@
+/******************************************************************************
+* Copyright (C) 2017 by Ben Heberlein
+*
+* Redistribution, modification or use of this software in source or binary
+* forms is permitted as long as the files maintain this copyright. This file
+* was created for the University of Colorado Boulder course Advanced Practical
+* Embedded Software Development. Ben Heberlein and the University of Colorado 
+* are not liable for any misuse of this material.
+*
+*******************************************************************************/
+/**
+ * @file memsort_test.c
+ * @brief A program for testing the memsort system call
+ * 
+ * This program tests the memsort sytsem call. Check the dmesg logs for
+ * system call output.
+ *
+ * @author Ben Heberlein
+ * @date September 17 2017
+ * @version 1.0
+ *
+ */
+
+#include <stdio.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <stdint.h>
+
+#define MEMSORT_NUM 500
+#define BUF_SIZE 256
+
+int main() {
+    int32_t buf[BUF_SIZE];
+    int32_t sbuf[BUF_SIZE];
+    int32_t size = BUF_SIZE;
+
+    printf("About to call memsort system call...\n");
+    int32_t ret = syscall(MEMSORT_NUM, buf, size, sbuf);
+    printf("Returned from memsort system call with error %d...\n", ret);
+    
+    return 0;
+}
